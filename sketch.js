fa93056fd42b3e77bf4373277e3c577aad3bcf91
Prisha12 <88835110+Prisha12@users.noreply.github.com>
2021-09-18 17:35:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+var a=prompt("enter the first number")
+var b=prompt("enter the second number")
+let temp;
+temp=a;
+a=b;
+b=temp;
+console.log("the number after swapping"+a);
+console.log("the number after swapping"+b);
+console.log("hello");
